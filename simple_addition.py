af1433082d16344ecbb7488c821c96d421925a2e
nguyen-thanh-1 <ntthanh14052005@gmail.com>
2026-09-18 12:35:31 +0700

    feat: add AOD histogram analysis notebook and report files for IMP302

---
+++ simple_addition.py
@@ -0,0 +1,11 @@
+def tinh_cong(a, b):
+    """Cộng hai số và trả về kết quả."""
+    return a + b
+
+
+so_thu_nhat = 5
+so_thu_hai = 3
+
+ket_qua = tinh_cong(so_thu_nhat, so_thu_hai)
+
+print(f"{so_thu_nhat} + {so_thu_hai} = {ket_qua}")
